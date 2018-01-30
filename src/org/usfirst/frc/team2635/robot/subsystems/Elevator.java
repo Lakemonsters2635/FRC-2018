4c62e246fea1a7185e22285e5ee0ee1892bae66b
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2018-01-29 20:13:07 -0800

    Code for Elevator
    Just code for elevator, not in robot.java, and needs to be created in
    robot.java    TB

--- src/org/usfirst/frc/team2635/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team2635/robot/subsystems/Elevator.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2635.robot.subsystems;
 
 import org.usfirst.frc.team2635.robot.RobotMap;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -17,12 +18,112 @@ public class Elevator extends Subsystem {
 	WPI_TalonSRX lowerMotor1; 
 	WPI_TalonSRX lowerMotor2; 
 	
+	//Arbitrary/Random encoder values for height of locations
+	//Ground:0
+	//Switch:2000
+	//Scale:5000
+	//Climb:7000
+	
+	//Arbitrary/Random encoder values for max elevator heights
+	//Lower: 4000
+	//Upper: 3000
+	
+	int[] heightArray;
+	int targetHeightIndex;
+	double lowerElevatorMax;
+	double upperElevatorMax;
+	
+	public Elevator() {
+		upperMotor  = new WPI_TalonSRX(RobotMap.ELEVATOR_UPPER_MOTOR_CHANNEL);
+    	lowerMotor1 = new WPI_TalonSRX(RobotMap.ELEVATOR_LOWER_MOTOR1_CHANNEL);
+    	lowerMotor2 = new WPI_TalonSRX(RobotMap.ELEVATOR_LOWER_MOTOR2_CHANNEL);
+    	
+    	lowerMotor2.follow(lowerMotor1);
+    	
+    	encoderStart();
+    	//TODO FAKE VALUES, UPDATE TO ACCURATE VALUES
+    	int groundHeight = 0;
+    	int switchHeight = 2000;
+    	int scaleHeight = 5000;
+    	int climbHeight = 7000;
+    	
+    	lowerElevatorMax = 4000;
+    	upperElevatorMax = 3000;
+    	//END FAKE VALUES
+    	
+    	targetHeightIndex = 0;
+    	heightArray = new int[] {groundHeight, switchHeight, scaleHeight, climbHeight};
+	}
+	
+	public void motorControl() {
+		double height = heightArray[targetHeightIndex];
+		double lowerHeight = 0;
+		double upperHeight = 0;
+		
+		if(height>upperElevatorMax) {
+			lowerHeight = upperElevatorMax;
+			upperHeight = height - upperElevatorMax;
+		} else {
+			upperHeight = 0;
+			lowerHeight = height;
+		}
+		
+		lowerMotor1.set(ControlMode.MotionMagic, lowerHeight);
+		upperMotor.set(ControlMode.MotionMagic, upperHeight);
+	}
+	
+	public boolean elevatorUp() {
+		if(targetHeightIndex < 4) {
+			targetHeightIndex += 1;
+		}
+		return true;
+	}
+	
+	public boolean elevatorDown() {
+		if(targetHeightIndex > 0) {
+			targetHeightIndex -= 1;
+		}
+		return true;
+	}
+	
+	public double currentHeight() {
+		
+		double lowerHeight = lowerMotor1.getSelectedSensorPosition(0);
+		double upperHeight = upperMotor.getSelectedSensorPosition(0);
+		
+		double totalHeight = lowerHeight+upperHeight;
+		
+		return totalHeight;
+	}
+	
+	private void encoderStart() {
+		upperMotor.setSelectedSensorPosition(0, 0, 0);
+    	upperMotor.config_kP(0, 5, 0);
+    	upperMotor.config_kI(0, 0, 0);
+    	upperMotor.config_kD(0, 0, 0);
+    	upperMotor.config_kF(0, 0, 0);
+    	
+    	lowerMotor1.setSelectedSensorPosition(0, 0, 0);
+    	lowerMotor1.config_kP(0, 5, 0);
+    	lowerMotor1.config_kI(0, 0, 0);
+    	lowerMotor1.config_kD(0, 0, 0);
+    	lowerMotor1.config_kF(0, 0, 0);
+    	
+    	
+    	lowerMotor1.selectProfileSlot(0, 0);
+    	upperMotor.selectProfileSlot(0, 0);
+    	
+    	lowerMotor1.configMotionAcceleration(100, 0);
+    	upperMotor.configMotionAcceleration(100, 0);
+    	
+    	lowerMotor1.configMotionCruiseVelocity(100, 0);
+    	upperMotor.configMotionCruiseVelocity(100, 0);
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    	upperMotor  = new WPI_TalonSRX(RobotMap.ELEVATOR_UPPER_MOTOR_CHANNEL);
-    	lowerMotor1 = new WPI_TalonSRX(RobotMap.ELEVATOR_LOWER_MOTOR1_CHANNEL);
-    	lowerMotor2 = new WPI_TalonSRX(RobotMap.ELEVATOR_LOWER_MOTOR2_CHANNEL);
+    	
     }
     
 }
